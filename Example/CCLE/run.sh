adbd89017016907f5d7306ee2846042fbfdf0c07
bsml320 <peilin.jia@gmail.com>
2018-10-10 11:35:26 -0500

    Create run.sh

---
+++ Example/CCLE/run.sh
@@ -0,0 +1,1 @@
+java -jar CNet.jar model=glm X=Erlotinib.X.txt Y=Erlotinib.Y.txt net=PC.interact.txt output=Erlotinib.modules.MUT.txt r_include=0.2 r_exclude=0.1 permutation=true keep_permutation=true 2>log &
